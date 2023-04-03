c7c3bbee6a4ddda8dc65c66ac52a3975a7199fe8
Никита Баранов <124291854+Borbos5@users.noreply.github.com>
2023-04-03 21:38:47 +0300

    Add files via upload

---
+++ script_dz5.js
@@ -0,0 +1,18 @@
+let arr = ["1", "2", "3"];   //1
+alert(arr.length);
+
+
+
+let arr1 = ["Nikita", "Maksim", "Jhon", "Danya", "Andrew", "Olga", "Eugene", "Pavel", "Dima", "Kolya"];
+alert(arr1[0]); //2
+alert(arr1[3]);
+alert(arr1[7]);
+
+
+
+
+let arr2 = [];
+arr2[1] = '10';
+arr2[2] = '8';
+arr2[5] = '100';
+console.log(arr2)
